fb0ea8c322c53f4035de6a4120e550b58b1b9926
Thomas Drevon <thomas@drevon.no>
2024-09-23 13:25:40 +0200

    Add supabase db connection check

--- src/lib/server/db/supabase.js
+++ src/lib/server/db/supabase.js
@@ -5,3 +5,13 @@ const supabaseUrl = PUBLIC_SUPABASE_URL
 const supabaseAnonKey = PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+export const pingSupabase = async () => {
+  const { data, error } = await supabase.from('Assessment').select('*', { count: 'exact' })
+  if (error) {
+    console.error('Mjauski :/ Failed to connect to database', error)
+    throw error
+  } else {
+    console.log('Supabase is up and running')
+  }
+}
--- src/routes/(marketing)/+page.server.js
+++ src/routes/(marketing)/+page.server.js
@@ -1,8 +1,12 @@
+import { pingSupabase } from '$lib/server/db/supabase.js'
 import { getTeachingGroupMemberships } from '$lib/server/db/user.js'
 
 /** @type {import('./$types').PageServerLoad} */
 export const load = async ({ params, parent, cookies }) => {
 	const parentData = await parent()
+
+	// verify that the supabase client is working
+	await pingSupabase()
 
 	const selectedTestUserId = cookies.get('selectedTestUserId')
 	const { data, error } = await getTeachingGroupMemberships({ userId: selectedTestUserId })
